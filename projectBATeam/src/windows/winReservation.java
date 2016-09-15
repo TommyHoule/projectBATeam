16ae756ef22e8f13c8d483313ad57a9d46b7a257
1226065 <1226065@172.18.8.0>
2016-09-15 10:39:42 -0400

    test999

--- projectBATeam/src/windows/winReservation.java
+++ projectBATeam/src/windows/winReservation.java
@@ -19,30 +19,39 @@ import java.awt.SystemColor;
 import javax.swing.JTable;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JSplitPane;
-import javax.swing.JSeparator;
-import javax.swing.SwingConstants;
 
 public class winReservation extends winHeritage {
 
 	private JPanel contentPane;
-	private JPanel panelChambre;
+	private JPanel panelReservationDeGauche;
+	private JLabel lblNoClient;
+	private JTextField txtNoClient;
 	
-	private ctrlChambre leControllerChambre ;
 	private winReservation instance;
-	public JTable tProduit;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_20;
-	private JTextField textField_21;
-	private JTextField textField_22;
-	private JTextField textField_23;
+	public JTable tableReservationDe;
+	private JPanel panelReservationDeDroite;
+	private JLabel lblNoreservation;
+	private JTextField txtNoReser;
+	private JLabel lblNom;
+	private JTextField txtNom;
+	private JLabel lblAdresse;
+	private JTextField txtAdresse;
+	private JLabel lblTlphone;
+	private JTextField txtTelephone;
+	private JTextField txtFax;
+	private JLabel lblFax;
+	private JLabel lblTypeDeCarte;
+	private JTextField txtTypeCarte;
+	private JLabel lblExpiration;
+	private JTextField txtExpiration;
+	private JLabel lblSoldeD;
+	private JTextField txtSoldeDu;
+	private JLabel lblRservLe;
+	private JLabel lblDateDeDbut;
+	private JLabel lblDateDeFin;
+	private JTextField txtDateReser;
+	private JTextField txtDateDebut;
+	private JTextField txtDateFin;
 
 
 	/**
@@ -153,95 +162,92 @@ public class winReservation extends winHeritage {
 	}
 
 	private void ModeConsultation() {
+		  
+		panelReservationDeGauche = new JPanel();
+		panelReservationDeGauche.setBackground(SystemColor.windowBorder);
+		panelReservationDeGauche.setBounds(182, 108, 469, 205);
+		getContentPane().add(panelReservationDeGauche);
+		panelReservationDeGauche.setLayout(null);
 		
-		leControllerChambre = new ctrlChambre(instance);
+		lblNoClient = new JLabel("Client no :");
+		lblNoClient.setBounds(20, 10, 77, 16);
+		panelReservationDeGauche.add(lblNoClient);
 		
-	   //Composants graphiques des chambres	    
-		panelChambre = new JPanel();
-		panelChambre.setBackground(SystemColor.windowBorder);
-		panelChambre.setBounds(204, 108, 469, 205);
-		getContentPane().add(panelChambre);
-		panelChambre.setLayout(null);
+		txtNoClient = new JTextField();
+		txtNoClient.setEditable(false);
+		txtNoClient.setBounds(125, 5, 64, 26);
+		panelReservationDeGauche.add(txtNoClient);
+		txtNoClient.setColumns(10);
 		
-		JLabel lblClientNo = new JLabel("Client no :");
-		lblClientNo.setBounds(20, 30, 72, 16);
-		panelChambre.add(lblClientNo);
+		lblNom = new JLabel("Nom :");
+		lblNom.setBounds(200, 10, 46, 16);
+		panelReservationDeGauche.add(lblNom);
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setColumns(10);
-		textField.setBounds(172, 25, 65, 26);
-		panelChambre.add(textField);
+		txtNom = new JTextField();
+		txtNom.setEditable(false);
+		txtNom.setColumns(10);
+		txtNom.setBounds(250, 5, 200, 26);
+		panelReservationDeGauche.add(txtNom);
 		
-		JLabel lblAdresse = new JLabel("Adresse :");
-		lblAdresse.setBounds(20, 65, 72, 16);
-		panelChambre.add(lblAdresse);
+		lblAdresse = new JLabel("Adresse :");
+		lblAdresse.setBounds(20, 50, 77, 16);
+		panelReservationDeGauche.add(lblAdresse);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setColumns(10);
-		textField_1.setBounds(168, 60, 258, 26);
-		panelChambre.add(textField_1);
+		txtAdresse = new JTextField();
+		txtAdresse.setEditable(false);
+		txtAdresse.setColumns(10);
+		txtAdresse.setBounds(125, 45, 200, 26);
+		panelReservationDeGauche.add(txtAdresse);
 		
-		JLabel lblNom = new JLabel("Nom :");
-		lblNom.setBounds(249, 30, 39, 16);
-		panelChambre.add(lblNom);
+		lblTlphone = new JLabel("Téléphone :");
+		lblTlphone.setBounds(20, 90, 77, 16);
+		panelReservationDeGauche.add(lblTlphone);
 		
-		textField_2 = new JTextField();
-		textField_2.setEditable(false);
-		textField_2.setColumns(10);
-		textField_2.setBounds(296, 25, 130, 26);
-		panelChambre.add(textField_2);
+		txtTelephone = new JTextField();
+		txtTelephone.setEditable(false);
+		txtTelephone.setColumns(10);
+		txtTelephone.setBounds(125, 85, 125, 26);
+		panelReservationDeGauche.add(txtTelephone);
 		
-		JLabel lblTelephone = new JLabel("Téléphone :");
-		lblTelephone.setBounds(20, 100, 86, 16);
-		panelChambre.add(lblTelephone);
+		txtFax = new JTextField();
+		txtFax.setEditable(false);
+		txtFax.setColumns(10);
+		txtFax.setBounds(325, 85, 125, 26);
+		panelReservationDeGauche.add(txtFax);
 		
-		textField_3 = new JTextField();
-		textField_3.setEditable(false);
-		textField_3.setColumns(10);
-		textField_3.setBounds(107, 95, 130, 26);
-		panelChambre.add(textField_3);
+		lblFax = new JLabel("Fax :");
+		lblFax.setBounds(275, 90, 35, 16);
+		panelReservationDeGauche.add(lblFax);
 		
-		JLabel lblFax = new JLabel("Fax :");
-		lblFax.setBounds(249, 100, 39, 16);
-		panelChambre.add(lblFax);
+		lblTypeDeCarte = new JLabel("Type de Carte :");
+		lblTypeDeCarte.setBounds(20, 130, 100, 16);
+		panelReservationDeGauche.add(lblTypeDeCarte);
 		
-		textField_4 = new JTextField();
-		textField_4.setEditable(false);
-		textField_4.setColumns(10);
-		textField_4.setBounds(296, 95, 130, 26);
-		panelChambre.add(textField_4);
+		txtTypeCarte = new JTextField();
+		txtTypeCarte.setEditable(false);
+		txtTypeCarte.setColumns(10);
+		txtTypeCarte.setBounds(125, 123, 200, 26);
+		panelReservationDeGauche.add(txtTypeCarte);
 		
-		JLabel lblTypeDeCarte = new JLabel("Type de Carte :");
-		lblTypeDeCarte.setBounds(20, 135, 110, 16);
-		panelChambre.add(lblTypeDeCarte);
+		lblExpiration = new JLabel("Expiration :");
+		lblExpiration.setBounds(20, 170, 100, 16);
+		panelReservationDeGauche.add(lblExpiration);
 		
-		textField_5 = new JTextField();
-		textField_5.setEditable(false);
-		textField_5.setColumns(10);
-		textField_5.setBounds(207, 130, 219, 26);
-		panelChambre.add(textField_5);
+		txtExpiration = new JTextField();
+		txtExpiration.setEditable(false);
+		txtExpiration.setColumns(10);
+		txtExpiration.setBounds(125, 165, 100, 26);
+		panelReservationDeGauche.add(txtExpiration);
 		
-		JLabel lblExpiration = new JLabel("Expiration :");
-		lblExpiration.setBounds(20, 170, 86, 16);
-		panelChambre.add(lblExpiration);
+		lblSoldeD = new JLabel("Solde dü :");
+		lblSoldeD.setBounds(250, 170, 64, 16);
+		panelReservationDeGauche.add(lblSoldeD);
 		
-		textField_6 = new JTextField();
-		textField_6.setEditable(false);
-		textField_6.setColumns(10);
-		textField_6.setBounds(132, 165, 105, 26);
-		panelChambre.add(textField_6);
-		
-		JLabel lblSoldeDu = new JLabel("Solde dû :");
-		lblSoldeDu.setBounds(249, 170, 65, 16);
-		panelChambre.add(lblSoldeDu);
-		
-		textField_7 = new JTextField();
-		textField_7.setEditable(false);
-		textField_7.setColumns(10);
-		textField_7.setBounds(315, 165, 110, 26);
-		panelChambre.add(textField_7);
+		txtSoldeDu = new JTextField();
+		txtSoldeDu.setEditable(false);
+		txtSoldeDu.setColumns(10);
+		txtSoldeDu.setBounds(325, 165, 100, 26);
+		panelReservationDeGauche.add(txtSoldeDu);
 		
 		
 		//composante graphique de Ayant
@@ -249,85 +255,78 @@ public class winReservation extends winHeritage {
 		scrollPane.setBounds(204, 370, 890, 205);
 		getContentPane().add(scrollPane);
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBackground(SystemColor.windowBorder);
-		panel.setBounds(709, 108, 385, 205);
-		getContentPane().add(panel);
+		panelReservationDeDroite = new JPanel();
+		panelReservationDeDroite.setLayout(null);
+		panelReservationDeDroite.setBackground(SystemColor.windowBorder);
+		panelReservationDeDroite.setBounds(685, 108, 440, 205);
+		getContentPane().add(panelReservationDeDroite);
 		
-		JLabel label_8 = new JLabel("No.Reservation :");
-		label_8.setBounds(42, 30, 110, 16);
-		panel.add(label_8);
+		lblNoreservation = new JLabel("No.Reservation :");
+		lblNoreservation.setBounds(70, 20, 115, 16);
+		panelReservationDeDroite.add(lblNoreservation);
 		
-		JLabel label_9 = new JLabel("Réservé le :");
-		label_9.setBounds(42, 65, 110, 16);
-		panel.add(label_9);
+		txtNoReser = new JTextField();
+		txtNoReser.setEditable(false);
+		txtNoReser.setColumns(10);
+		txtNoReser.setBounds(225, 15, 130, 26);
+		panelReservationDeDroite.add(txtNoReser);
 		
-		JLabel label_10 = new JLabel("Date de début :");
-		label_10.setBounds(42, 100, 110, 16);
-		panel.add(label_10);
+		lblRservLe = new JLabel("Réservé le :");
+		lblRservLe.setBounds(70, 70, 92, 16);
+		panelReservationDeDroite.add(lblRservLe);
 		
-		JLabel label_11 = new JLabel("Date de fin :");
-		label_11.setBounds(42, 135, 110, 16);
-		panel.add(label_11);
+		lblDateDeDbut = new JLabel("Date de début :");
+		lblDateDeDbut.setBounds(70, 120, 115, 16);
+		panelReservationDeDroite.add(lblDateDeDbut);
 		
-		textField_20 = new JTextField();
-		textField_20.setEditable(false);
-		textField_20.setColumns(10);
-		textField_20.setBounds(183, 25, 130, 26);
-		panel.add(textField_20);
+		lblDateDeFin = new JLabel("Date de fin :");
+		lblDateDeFin.setBounds(70, 170, 92, 16);
+		panelReservationDeDroite.add(lblDateDeFin);
 		
-		textField_21 = new JTextField();
-		textField_21.setEditable(false);
-		textField_21.setColumns(10);
-		textField_21.setBounds(183, 60, 130, 26);
-		panel.add(textField_21);
+		txtDateReser = new JTextField();
+		txtDateReser.setEditable(false);
+		txtDateReser.setColumns(10);
+		txtDateReser.setBounds(225, 65, 130, 26);
+		panelReservationDeDroite.add(txtDateReser);
 		
-		textField_22 = new JTextField();
-		textField_22.setEditable(false);
-		textField_22.setColumns(10);
-		textField_22.setBounds(183, 95, 130, 26);
-		panel.add(textField_22);
+		txtDateDebut = new JTextField();
+		txtDateDebut.setEditable(false);
+		txtDateDebut.setColumns(10);
+		txtDateDebut.setBounds(225, 115, 130, 26);
+		panelReservationDeDroite.add(txtDateDebut);
 		
-		textField_23 = new JTextField();
-		textField_23.setEditable(false);
-		textField_23.setColumns(10);
-		textField_23.setBounds(183, 130, 130, 26);
-		panel.add(textField_23);
-		
-		JLabel lblInformationSurLe = new JLabel("Information sur le client qui réserve :");
-		lblInformationSurLe.setBounds(204, 80, 269, 16);
-		getContentPane().add(lblInformationSurLe);
-		
-		JLabel lblInformationSurLa = new JLabel("Information sur la réservation :");
-		lblInformationSurLa.setBounds(709, 80, 269, 16);
-		getContentPane().add(lblInformationSurLa);
+		txtDateFin = new JTextField();
+		txtDateFin.setEditable(false);
+		txtDateFin.setColumns(10);
+		txtDateFin.setBounds(225, 165, 130, 26);
+		panelReservationDeDroite.add(txtDateFin);
 		
 		//Gestion de la souris sur le menu et la navigation
 		
 		btnPremier.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				leControllerChambre.Premier(instance);
+				//leControllerChambre.Premier(instance);
 			}
 		});
 		btnPrecedent.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				leControllerChambre.BonPrecedent(instance);
+				//leControllerChambre.BonPrecedent(instance);
 			}
 		});
 		btnDernier.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				leControllerChambre.Dernier(instance);
+				//leControllerChambre.Dernier(instance);
 			}
 		});
 		btnSuivant.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				leControllerChambre.BonSuivant(instance);
+				//leControllerChambre.BonSuivant(instance);
 			}
 		});
 	}
+
 }
