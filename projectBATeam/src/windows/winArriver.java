fc9710461d18aa98b52fbd4d81a93b82392f0153
Couvrette Benjamin <benjamin_couvrette@hotmail.com>
2016-09-15 08:44:56 -0400

    ma branche accepter pas

--- projectBATeam/src/windows/winArriver.java
+++ projectBATeam/src/windows/winArriver.java
@@ -10,12 +10,30 @@ import javax.swing.JTable;
 import java.awt.Component;
 import javax.swing.Box;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.TableColumn;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class winArriver extends winHeritage {
 
 	private JPanel contentPane;
-	private JTable table;
+	private JTextField textFieldClientNo;
+	private JTextField textFieldClientNom;
+	private JTextField textFieldAdresse;
+	private JTextField textFieldTelephone;
+	private JTextField textFieldFax;
+	private JTextField textFieldNumeroChambre;
+	private JTextField textFieldNoReser;
+	private JTextField textFieldReservLe;
+	private JTextField textFieldDateDebut;
+	private JTextField textFieldDateFin;
+	private JTextField textFieldClientNoReserv;
+	private JTextField textFieldNomReserv;
 
 	/**
 	 * Launch the application.
@@ -39,29 +57,136 @@ public class winArriver extends winHeritage {
 	public winArriver() {
 		
 		JPanel panelClient = new JPanel();
-		panelClient.setBounds(176, 88, 525, 280);
+		panelClient.setBackground(SystemColor.windowBorder);
+		panelClient.setBounds(176, 88, 469, 280);
 		getContentPane().add(panelClient);
 		panelClient.setLayout(null);
 		
+		textFieldClientNo = new JTextField();
+		textFieldClientNo.setBounds(97, 40, 130, 26);
+		panelClient.add(textFieldClientNo);
+		textFieldClientNo.setColumns(10);
+		
+		JLabel lblClientNo = new JLabel("Client No:");
+		lblClientNo.setBounds(30, 45, 63, 16);
+		panelClient.add(lblClientNo);
+		
+		textFieldClientNom = new JTextField();
+		textFieldClientNom.setBounds(296, 40, 130, 26);
+		panelClient.add(textFieldClientNom);
+		textFieldClientNom.setColumns(10);
+		
+		JLabel lblNom = new JLabel("Nom:");
+		lblNom.setBounds(250, 45, 34, 16);
+		panelClient.add(lblNom);
+		
+		JLabel lblAdresse = new JLabel("Adresse:");
+		lblAdresse.setBounds(39, 104, 54, 16);
+		panelClient.add(lblAdresse);
+		
+		textFieldAdresse = new JTextField();
+		textFieldAdresse.setBounds(97, 99, 329, 26);
+		panelClient.add(textFieldAdresse);
+		textFieldAdresse.setColumns(10);
+		
+		JLabel lblTlphone = new JLabel("Téléphone:");
+		lblTlphone.setBounds(24, 163, 69, 16);
+		panelClient.add(lblTlphone);
+		
+		textFieldTelephone = new JTextField();
+		textFieldTelephone.setBounds(97, 158, 130, 26);
+		panelClient.add(textFieldTelephone);
+		textFieldTelephone.setColumns(10);
+		
+		JLabel lblFax = new JLabel("Fax:");
+		lblFax.setBounds(258, 163, 26, 16);
+		panelClient.add(lblFax);
+		
+		textFieldFax = new JTextField();
+		textFieldFax.setBounds(296, 158, 130, 26);
+		panelClient.add(textFieldFax);
+		textFieldFax.setColumns(10);
+		
+		textFieldNumeroChambre = new JTextField();
+		textFieldNumeroChambre.setBounds(156, 218, 130, 26);
+		panelClient.add(textFieldNumeroChambre);
+		textFieldNumeroChambre.setColumns(10);
+		
+		JLabel lblNumroChambre = new JLabel("Numéro Chambre:");
+		lblNumroChambre.setBounds(30, 223, 114, 16);
+		panelClient.add(lblNumroChambre);
+		
 		JPanel panelReservation = new JPanel();
-		panelReservation.setBounds(733, 88, 381, 280);
+		panelReservation.setBackground(SystemColor.windowBorder);
+		panelReservation.setBounds(674, 88, 440, 280);
 		getContentPane().add(panelReservation);
 		panelReservation.setLayout(null);
 		
-		JPanel panelN = new JPanel();
-		panelN.setBounds(176, 391, 938, 243);
-		getContentPane().add(panelN);
+		JLabel lblNoReservation = new JLabel("No. Réservation:");
+		lblNoReservation.setBounds(26, 43, 103, 16);
+		panelReservation.add(lblNoReservation);
 		
-		table = new JTable();
-		panelN.add(table);
+		textFieldNoReser = new JTextField();
+		textFieldNoReser.setBounds(141, 38, 179, 26);
+		panelReservation.add(textFieldNoReser);
+		textFieldNoReser.setColumns(10);
+		
+		JLabel lblReservLe = new JLabel("Réservé le:");
+		lblReservLe.setBounds(62, 103, 67, 16);
+		panelReservation.add(lblReservLe);
+		
+		textFieldReservLe = new JTextField();
+		textFieldReservLe.setBounds(141, 98, 179, 26);
+		panelReservation.add(textFieldReservLe);
+		textFieldReservLe.setColumns(10);
+		
+		JLabel lblDateDeDebut = new JLabel("Date de début:");
+		lblDateDeDebut.setBounds(37, 163, 92, 16);
+		panelReservation.add(lblDateDeDebut);
+		
+		textFieldDateDebut = new JTextField();
+		textFieldDateDebut.setBounds(141, 158, 179, 26);
+		panelReservation.add(textFieldDateDebut);
+		textFieldDateDebut.setColumns(10);
+		
+		JLabel lblDateDeFin = new JLabel("Date de fin:");
+		lblDateDeFin.setBounds(56, 198, 73, 16);
+		panelReservation.add(lblDateDeFin);
+		
+		textFieldDateFin = new JTextField();
+		textFieldDateFin.setBounds(141, 196, 179, 26);
+		panelReservation.add(textFieldDateFin);
+		textFieldDateFin.setColumns(10);
+		
+		JLabel lblClientNoReserv = new JLabel("Client no:");
+		lblClientNoReserv.setBounds(26, 239, 61, 16);
+		panelReservation.add(lblClientNoReserv);
+		
+		textFieldClientNoReserv = new JTextField();
+		textFieldClientNoReserv.setBounds(99, 234, 78, 26);
+		panelReservation.add(textFieldClientNoReserv);
+		textFieldClientNoReserv.setColumns(10);
+		
+		JLabel lblNomReserv = new JLabel("Nom:");
+		lblNomReserv.setBounds(189, 239, 34, 16);
+		panelReservation.add(lblNomReserv);
+		
+		textFieldNomReserv = new JTextField();
+		textFieldNomReserv.setBounds(235, 234, 130, 26);
+		panelReservation.add(textFieldNomReserv);
+		textFieldNomReserv.setColumns(10);
 		
 		JLabel lblInforClient = new JLabel("Information sur le client");
 		lblInforClient.setBounds(176, 60, 152, 16);
 		getContentPane().add(lblInforClient);
 		
 		JLabel lblInformationSurLa = new JLabel("Information sur la réservation");
-		lblInformationSurLa.setBounds(733, 60, 192, 16);
+		lblInformationSurLa.setBounds(674, 60, 192, 16);
 		getContentPane().add(lblInformationSurLa);
+		
+		JScrollPane scrollPaneZoneN = new JScrollPane();
+		scrollPaneZoneN.setBounds(176, 392, 938, 235);
+		getContentPane().add(scrollPaneZoneN);
 		/*setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
